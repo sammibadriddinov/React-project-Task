25d5afbd5b79144c154462a0009ecf76f273a835
sammibadriddinov <76088408+sammibadriddinov@users.noreply.github.com>
2021-05-02 17:17:07 +0500

    start project and realize new item

--- src/App.js
+++ src/App.js
@@ -8,8 +8,42 @@ function App() {
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
-
-  return <h2>grocery bud setup</h2>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name) {
+      // Alert display
+    } else if (name && isEditing) {
+      // edit
+    } else {
+      const newItem = { id: new Date().getTime().toString(), title: name };
+      setList([...list, newItem]);
+      setName("");
+    }
+  };
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}>
+        {alert.show && <Alert />}
+        <h3>Список Заметки</h3>
+        <div className="form-control">
+          <input
+            type="text"
+            className="grocery"
+            placeholder="Место для заметки"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type="submit" className="submit-btn">
+            {isEditing ? "Изменить" : "Добавить"}
+          </button>
+        </div>
+      </form>
+      <div className="grocery-container">
+        <List />
+        <button className="clear-btn">Очистить</button>
+      </div>
+    </section>
+  );
 }
 
 export default App;
