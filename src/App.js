7734b31cca3f29ad31ff0bdd470a2b9f692e3778
sammibadriddinov <76088408+sammibadriddinov@users.noreply.github.com>
2021-05-02 14:18:48 +0500

    set useState

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
+
+function App() {
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+
+  return <h2>grocery bud setup</h2>;
+}
+
+export default App;
